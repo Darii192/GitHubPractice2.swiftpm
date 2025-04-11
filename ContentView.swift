2115f3943fcf5c7d9c47678b29876b752c80ded3
Bibi <bgonzalez7140@stu.d214.org>
2025-04-11 13:38:11 -0500

    changed variables.

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number2: Double
-    @State var number1: Double
-    @State var answer: Double
+    @State var number2 = 0
+    @State var number1 = 0
+    @State var answer = 0
     var body: some View {
         
         TextField("Number 2", value: $number2, format: .number)

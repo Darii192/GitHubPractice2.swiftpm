37e9677fd067d6d8641c5d10ceebf4fcd439eef2
Bibi <bgonzalez7140@stu.d214.org>
2025-04-11 13:21:22 -0500

    Added variable.

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,11 @@ import SwiftUI
 struct ContentView: View {
     @State var number2: Double
     var body: some View {
-        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
     }
 }
